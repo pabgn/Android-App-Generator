a86705919be7699f66c43b4204507c7ee4049db6
José Miguel Molina Arboledas <rd4091@gmail.com>
2012-08-14 23:52:39 +0200

    Dynamic items table.

--- app.py
+++ app.py
@@ -3,10 +3,11 @@
 
 from flask import Flask, render_template, request, url_for 
 app = Flask(__name__)
+objects = [('Layouts', ['LinearLayout', 'AbsoluteLayout', 'RelativeLayout']), ('Buttons', ['Button'])]
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', objects=objects)
 	
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=3434)

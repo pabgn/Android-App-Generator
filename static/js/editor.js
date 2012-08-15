e6cdab0905c07f49a24f9f0b17c6a62f24394524
José Miguel Molina Arboledas <rd4091@gmail.com>
2012-08-15 11:32:43 +0200

    Cambiados " por '

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,12 +1,12 @@
 var editor = {
 	dragging:false,
-	layoutSelected:"elements",
+	layoutSelected:'elements',
 	count:0,
 	elements:{},
 	initDraggable: function(){
 		$('.object').draggable({  
 			opacity: 0.7, 
-			helper: "clone", 
+			helper: 'clone', 
 			revert:true,  
 		});	
 		$('.object').bind('drag', function(ev, ui){
@@ -21,41 +21,41 @@ var editor = {
 	dragMonitor: function(object, original, stop){
 		editor.dragging = true;
 		if (stop) {
-			$("#"+editor.layoutSelected).append(Objects[$(original).attr('id')]['HTML']);
+			$('#'+editor.layoutSelected).append(Objects[$(original).attr('id')]['HTML']);
 			//Save new element
-			elements["e"+editor.count]=Objects[$(original).attr('id')];
+			elements['e'+editor.count]=Objects[$(original).attr('id')];
 			editor.count++;
 			//
-			$("#"+editor.layoutSelected).css("background-color", "");
+			$('#'+editor.layoutSelected).css('background-color', '');
 			editor.dragging = false;
 		}
 	},
 	showLayouts:function(){
-			$(".layout").css("border", "2px dotted white");			
-			$(".layout").css("height", "40px");			
+			$('.layout').css('border', '2px dotted white');			
+			$('.layout').css('height', '40px');			
 	},
 	hideLayouts:function(){
-		$(".layout").css("border", "");			
-		$(".layout").css("height", "100%");				
+		$('.layout').css('border', '');			
+		$('.layout').css('height', '100%');				
 	},
 	hoverDetectors: function(){
 		//Main Layout (elements)
-		$("#elements").hover(function() {
-				$("#console").html("Inside Elements");
-		 		editor.layoutSelected = "elements";
-				$(this).css("background-color", "#515151");	
+		$('#elements').hover(function() {
+				$('#console').html('Inside Elements');
+		 		editor.layoutSelected = 'elements';
+				$(this).css('background-color', '#515151');	
 		}, function() {
-				$("#console").html("");
-				$(this).css("background-color", "");			
+				$('#console').html('');
+				$(this).css('background-color', '');			
 		});
 		//Layouts:
-		$(".layout").hover(function(){
-				$("#console").html("Inside Layout");
-				editor.layoutSelected=$(this).attr("id");
-				$(this).css("background-color", "#515151");
+		$('.layout').hover(function(){
+				$('#console').html('Inside Layout');
+				editor.layoutSelected=$(this).attr('id');
+				$(this).css('background-color', '#515151');
 			}, function(){
-				$("#console").html("");
-				$(this).css("background-color", "");
+				$('#console').html('');
+				$(this).css('background-color', '');
 			}
 		);
 	},	

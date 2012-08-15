61cd5c6f9c4ee952447b8219d7198ace87bc0f8e
pabgn <pablo@garcianieto.me>
2012-08-15 12:43:36 +0200

    Changes.

--- static/js/objects.js
+++ static/js/objects.js
@@ -1,35 +1,21 @@
 var Objects = {
-	Elements = {}
-	LinearLayout:{
-	 	HTML:"<div class='layout' id='LinearLayout%d' style='width:100%;'>",
+
+		 LinearLayout:{
+	 	HTML:"<div class='layout' id='LinearLayout"+Objects.getId()+"' style='width:100%;'>",
 	 	value:"",
-	 	name: 'LinearLayout %d',
-	 	id:"",
+	 	id:Objects.getId(),
 	 	style:"",
-	 	action:""
-	},
-	RelativeLayout:{
-	  	HTML:"<div class='layout' id='RelativeLayout%d' style='width:100%;'>",
-	  	value:"",
-	  	name: 'RelativeLayout %d',
-	  	id:"",
-	  	style:"",
-	  	action:""
-	},
-	AbsoluteLayout:{
-	   	HTML:"<div class='layout' id='AbsoluteLayout%d' style='width:100%;'>",
-	   	value:"",
-	   	name: 'AbsoluteLayout %d',
-	   	id:"",
-	   	style:"",
-	   	action:""
-	},
-	Button:{
-	 	HTML:"<input type='button' id='Button%d' value='Button'>",
+	 	action:"",
+	 	inside:{}
+	 },
+	 Button:{
+	 	HTML:"<input type='button' value='Button'>",
 	 	value:"",
 	 	id:"",
 	 	name: 'Button %d',
 	 	style:"",
 	 	action:""
 	}
+	 		
+	
 }
